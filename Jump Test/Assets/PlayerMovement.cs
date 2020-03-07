ca423365f30bf6cfbbebd9261bbf3f6ec2622b5f
drew199321 <43482659+drew199321@users.noreply.github.com>
2020-03-07 00:59:00 -0800

    Crap Jump Test
    A Jump Test done in spare time with minimal functionality: Walk, Jump, Lookup, Lookdown all funcitonal

---
+++ Jump Test/Assets/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    public System.Timers.Timer _delayTimer;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    bool jump = false;
+    bool lookDown = false;
+    bool lookUp = false;
+
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJump", false);
+        animator.SetBool("LandFrame", false);
+    }
+
+    // Update is called once per frame
+    void Update(){
+       
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJump", true);
+        }
+
+        if (Input.GetButtonDown("Up"))
+        {
+            lookUp = true;
+            animator.SetBool("IsLookUp", true);
+        }
+        else if (Input.GetButtonUp("Up"))
+        {
+            lookUp = false;
+            animator.SetBool("IsLookUp", false);
+        }
+
+        if (Input.GetButtonDown("Down"))
+        {
+            lookDown = true;
+            animator.SetBool("IsLookDown", true);
+        } else if(Input.GetButtonUp("Down"))
+        {
+            lookDown = false;
+            animator.SetBool("IsLookDown", false);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        //Character Movement
+        controller.Move(horizontalMove*Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
