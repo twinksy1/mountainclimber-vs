88cab4edc20968642e2e147293869502068c9683
josh-logue <jlogue951@gmail.com>
2020-03-30 11:06:06 -0700

    update

---
+++ MountainClimber-VS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    public System.Timers.Timer _delayTimer;
+
+    float horizontalMove = 0f;
+    float verticalMove = 0f;
+    bool jump = false;
+    bool lookDown = false;
+    bool lookUp = false;
+    bool locationLock = false;
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJump", false);
+        animator.SetBool("LandFrame", false);
+        animator.SetBool("IsFalling", false);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        bool vectorbool = controller.getVectorBoolY();
+       
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if(horizontalMove != 0)
+        {
+            locationLock = true;
+        }
+        else
+        {
+            locationLock = false;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJump", true);
+        }
+
+        if (Input.GetButtonDown("Up") && locationLock == false)
+        {
+            lookUp = true;
+            animator.SetBool("IsLookUp", true);
+        }
+        else if (Input.GetButtonUp("Up"))
+        {
+            lookUp = false;
+            animator.SetBool("IsLookUp", false);
+        }
+
+        if (Input.GetButtonDown("Down") && locationLock == false)
+        {
+            lookDown = true;
+            animator.SetBool("IsLookDown", true);
+        } else if(Input.GetButtonUp("Down"))
+        {
+            lookDown = false;
+            animator.SetBool("IsLookDown", false);
+        }
+
+        if(vectorbool == true)
+        {
+            animator.SetBool("IsFalling", false);
+        }
+        else
+        {
+            animator.SetBool("IsJump", false);
+            animator.SetBool("IsFalling", true);
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        //Character Movement
+        controller.Move(horizontalMove*Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
