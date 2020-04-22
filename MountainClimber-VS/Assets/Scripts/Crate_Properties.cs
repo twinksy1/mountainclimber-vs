dad84cf88fd1de6c639fe0cde79b9f64e25b1401
twinksy1 <55815806+twinksy1@users.noreply.github.com>
2020-04-21 21:35:42 -0700

    Added crate
    Added a crate object that plays a breaking animation when an object with the player tag collides with it. Also fixed a visual glitch where the score UI was being blocked by the platforms and player.

---
+++ MountainClimber-VS/Assets/Scripts/Crate_Properties.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate_Properties : MonoBehaviour
+{
+    public Animator animator;
+    public BoxCollider2D box_collider;
+    public Rigidbody2D rb;
+
+    bool collided = false;
+    public float secondsTilDeath = 0.5f;
+
+    void Start()
+    {
+        animator.enabled = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        Debug.Log("COLLISION WITH CRATE");
+        if (collisionInfo.gameObject.tag == "Player")
+        {
+            collided = true;
+            //movement.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if(collided)
+        {
+            animator.enabled = true;
+            animator.Play("Crate_Break");
+            box_collider.enabled = false;
+            Destroy(rb);
+            Destroy(gameObject, secondsTilDeath);
+        }
+    }
+}
